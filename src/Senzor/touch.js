188f2002a9d3543a563113a8e5b8f0353c70abd5
botanrobert <botanrobert@gmail.com>
2017-04-07 20:00:15 +0300

    adaugat touch

---
+++ src/Senzor/touch.js
@@ -0,0 +1,20 @@
+window.addEventListener("touchstart", on_touch_start);
+
+var touch_colors = ["#FF0000","#00FF00","#0000FF","#00FF00"];
+
+function on_touch_start(evt)
+{
+	var touches = evt.changedTouches;
+
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+
+	for(var i = 0; i < touches.length; i++){
+
+		ctx.beginPath();
+		ctx.strokeStyle = touch_colors[i];
+		ctx.arc(touches[i].pageX, touches[i].pageY, 35, 0, 2 * Math.PI);
+		ctx.stroke();
+	}
+
+}
